de699c318d691a14f0c0806f4621ae88ddcfce70
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-01-18 18:59:45 +0530

    Update theme

--- app/src/main/java/com/rharshit/winddown/MainActivity.java
+++ app/src/main/java/com/rharshit/winddown/MainActivity.java
@@ -2,6 +2,7 @@ package com.rharshit.winddown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
