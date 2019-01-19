be11f43ccec264e118a4a8959f2280d7b9ef0788
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-01-19 16:46:00 +0530

    Phone: Add dummy activity

---
+++ app/src/main/java/com/rharshit/winddown/Phone/Phone.java
@@ -0,0 +1,15 @@
+package com.rharshit.winddown.Phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rharshit.winddown.R;
+
+public class Phone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone);
+    }
+}
