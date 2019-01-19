38258aa705803f75c185ba19a0950a2415a7c3dd
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-01-19 15:17:22 +0530

    UI: Add horizontal scroll

--- app/src/main/java/com/rharshit/winddown/MainActivity.java
+++ app/src/main/java/com/rharshit/winddown/MainActivity.java
@@ -3,13 +3,21 @@ package com.rharshit.winddown;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
+    HorizontalScrollView hsView;
+
+    void init(){
+        hsView = (HorizontalScrollView) findViewById(R.id.hsMainScrollView);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        init();
     }
 }
