a45cf4e0c843dd08ed2528723445252ece43af08
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-01-20 11:28:42 +0530

    Music: Initialize

---
+++ app/src/main/java/com/rharshit/winddown/Music/Music.java
@@ -0,0 +1,15 @@
+package com.rharshit.winddown.Music;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rharshit.winddown.R;
+
+public class Music extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+}
