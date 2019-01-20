a7c1a352b25f077ad2c8935f0e5c3f849e17db39
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-01-20 11:27:11 +0530

    Galerry: Initialize

---
+++ app/src/main/java/com/rharshit/winddown/Gallery/Gallery.java
@@ -0,0 +1,15 @@
+package com.rharshit.winddown.Gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rharshit.winddown.R;
+
+public class Gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
