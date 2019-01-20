c0ce91c6fdbbf6f90c6812b30adcf3cc9a766c5c
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-01-20 11:21:24 +0530

    Contacts: Initialize

---
+++ app/src/main/java/com/rharshit/winddown/Contacts/Contacts.java
@@ -0,0 +1,15 @@
+package com.rharshit.winddown.Contacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rharshit.winddown.R;
+
+public class Contacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
