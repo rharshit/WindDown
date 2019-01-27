b2bcea51553002631800e89fa883abbb52994718
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-01-27 16:53:30 +0530

    Notification: Add icons

--- app/src/main/java/com/rharshit/winddown/MainActivity.java
+++ app/src/main/java/com/rharshit/winddown/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -201,7 +203,13 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "New notification: " + n.getPackageName() + " GroupKey: " + n.getGroupKey()
                 + " Key: " + n.getKey() + " nGroup: " + n.getGroup() + " ID: " + n.getId()
                 + " Channel ID: " + n.getChannelId());
-        llNotification.addView(new NotificationView(mContext, n));
+        Drawable icon = null;
+        try {
+            icon = getPackageManager().getApplicationIcon(n.getPackageName());
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        llNotification.addView(new NotificationView(mContext, n, icon));
     }
 
     class NotificationReceiver extends BroadcastReceiver {
--- app/src/main/java/com/rharshit/winddown/UI/NotificationView.java
+++ app/src/main/java/com/rharshit/winddown/UI/NotificationView.java
@@ -1,8 +1,9 @@
 package com.rharshit.winddown.UI;
 
 import android.content.Context;
-import android.graphics.drawable.Icon;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -10,21 +11,36 @@ import android.widget.TextView;
 
 import com.rharshit.winddown.Util.Notification;
 
+import static android.content.ContentValues.TAG;
+
 public class NotificationView extends LinearLayout {
 
     private Notification notification;
 
-    public NotificationView(Context context, Notification notification) {
+    public NotificationView(Context context, Notification notification, Drawable icon) {
         super(context);
 
         this.setLayoutParams(new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         this.setOrientation(HORIZONTAL);
         this.notification = notification;
 
-        TextView packageName = new TextView(context);
-        packageName.setText(notification.getPackageName());
-        packageName.setTextSize(16.0f);
-        this.addView(packageName);
+        float dimen = 64 * getResources().getDisplayMetrics().density;
+
+        ImageView ivIcon = new ImageView(context);
+        ivIcon.setLayoutParams(new LayoutParams((int) dimen, (int) dimen));
+        ivIcon.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        ivIcon.setPadding(8,8,8,8);
+        if(icon!=null){
+            ivIcon.setImageDrawable(icon);
+        } else {
+            Log.e(TAG, "NotificationView: NULL Icon");
+        }
+        this.addView(ivIcon);
+
+//        TextView packageName = new TextView(context);
+//        packageName.setText(notification.getPackageName());
+//        packageName.setTextSize(16.0f);
+//        this.addView(packageName);
     }
 
     public String getGroupKey(){
