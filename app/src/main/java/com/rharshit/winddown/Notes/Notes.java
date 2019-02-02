3cdfef4ea3fdc6b7181286987de5f711d672b217
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-02-03 01:52:49 +0530

    Notes: Add new hamburger icon

--- app/src/main/java/com/rharshit/winddown/Notes/Notes.java
+++ app/src/main/java/com/rharshit/winddown/Notes/Notes.java
@@ -3,6 +3,7 @@ package com.rharshit.winddown.Notes;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -44,6 +45,10 @@ public class Notes extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
     }
 
     @Override
