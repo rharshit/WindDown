8dfd7fdca8cea9e19323319d3e699561af8c45f2
Rishabh Kumar Sinha <rishabhsinhaaa@gmail.com>
2019-03-10 10:24:03 +0530

    preview works but crashes if clicked

---
+++ app/src/main/java/com/rharshit/winddown/Gallery/GalleryPreview.java
@@ -0,0 +1,29 @@
+package com.rharshit.winddown.Gallery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.rharshit.winddown.R;
+
+import java.io.File;
+
+public class GalleryPreview extends AppCompatActivity {
+
+    ImageView GalleryPreviewImg;
+    String path;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.gallery_preview);
+        Intent intent = getIntent();
+        path = intent.getStringExtra("path");
+        GalleryPreviewImg = (ImageView) findViewById(R.id.GalleryPreviewImg);
+        Glide.with(GalleryPreview.this)
+                .load(new File(path)) // Uri of the picture
+                .into(GalleryPreviewImg);
+    }
+}
