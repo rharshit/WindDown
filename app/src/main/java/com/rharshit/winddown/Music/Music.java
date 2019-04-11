2827e02a83976274a0f102cf5a614ce5dbef8809
sahilvora10 <30970675+sahilvora10@users.noreply.github.com>
2019-04-11 13:01:51 +0530

    Music Added

--- app/src/main/java/com/rharshit/winddown/Music/Music.java
+++ app/src/main/java/com/rharshit/winddown/Music/Music.java
@@ -1,15 +1,166 @@
 package com.rharshit.winddown.Music;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rharshit.winddown.R;
 
-public class Music extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
 
+
+
+
+import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+
+
+        import android.app.Activity;
+        import android.media.MediaPlayer;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.view.View;
+
+        import android.widget.Button;
+        import android.widget.ImageButton;
+        import android.widget.ImageView;
+        import android.widget.SeekBar;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import java.util.concurrent.TimeUnit;
+
+
+public class Music extends Activity {
+    private ImageButton b1,b2,b3,b4;
+    private ImageView iv;
+    private MediaPlayer mediaPlayer;
+
+    private double startTime = 0;
+    private double finalTime = 0;
+
+    private Handler myHandler = new Handler();;
+    private int forwardTime = 5000;
+    private int backwardTime = 5000;
+    private SeekBar seekbar;
+    private TextView tx1,tx2,tx3;
+
+    public static int oneTimeOnly = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_music );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+
+        b1 = (ImageButton) findViewById(R.id.button);
+        b2 = (ImageButton) findViewById(R.id.button2);
+        b3 = (ImageButton)findViewById(R.id.button3);
+        b4 = (ImageButton)findViewById(R.id.button4);
+        iv = (ImageView)findViewById(R.id.imageView);
+
+        tx1 = (TextView)findViewById(R.id.textView2);
+        tx2 = (TextView)findViewById(R.id.textView3);
+        tx3 = (TextView)findViewById(R.id.textView4);
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        seekbar.setClickable(false);
+        b2.setEnabled(false);
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Playing sound",Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+
+                finalTime = mediaPlayer.getDuration();
+                startTime = mediaPlayer.getCurrentPosition();
+
+                if (oneTimeOnly == 0) {
+                    seekbar.setMax((int) finalTime);
+                    oneTimeOnly = 1;
+                }
+
+                tx2.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                        finalTime)))
+                );
+
+                tx1.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                        startTime)))
+                );
+
+                seekbar.setProgress((int)startTime);
+                myHandler.postDelayed(UpdateSongTime,100);
+                b2.setEnabled(true);
+                b3.setEnabled(false);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Pausing sound",Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+                b2.setEnabled(false);
+                b3.setEnabled(true);
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int)startTime;
+
+                if((temp+forwardTime)<=finalTime){
+                    startTime = startTime + forwardTime;
+                    mediaPlayer.seekTo((int) startTime);
+                    Toast.makeText(getApplicationContext(),"You have Jumped forward 5 seconds",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Cannot jump forward 5 seconds",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int)startTime;
+
+                if((temp-backwardTime)>0){
+                    startTime = startTime - backwardTime;
+                    mediaPlayer.seekTo((int) startTime);
+                    Toast.makeText(getApplicationContext(),"You have Jumped backward 5  seconds",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Cannot jump backward 5 seconds",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-}
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            tx1.setText(String.format("%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                    toMinutes((long) startTime)))
+            );
+            seekbar.setProgress((int)startTime);
+            myHandler.postDelayed(this, 100);
+        }
+    };}
